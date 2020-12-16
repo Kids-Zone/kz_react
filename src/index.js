a7137f1d42b0f16567a254a3d3b549cad9367ed6
bsmagennis42 <bsmagennis42@gmail.com>
2020-12-16 23:47:40 +0000

    Created starter with React cli. Converting static HTML to components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import KidZoneApp from './KidZoneApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <KidZoneApp />
   </React.StrictMode>,
   document.getElementById('root')
 );

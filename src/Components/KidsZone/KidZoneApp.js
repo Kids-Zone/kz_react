4b29f36a17403ee2598a47e1545d7cfeb7995f29
bsmagennis42 <bsmagennis42@gmail.com>
2020-12-21 10:12:12 +0000

    removed duplicated images

--- src/Components/KidsZone/KidZoneApp.js
+++ src/Components/KidsZone/KidZoneApp.js
@@ -7,7 +7,7 @@ const Header = props => <header class="">
             <div class="container">
                 <a class="navbar-brand" href="index.html">
                     <img src="../../images/bee.jpg" width="30" height="30" class="d-inline-block align-top" alt=""/>
-                        Kids Zone </a>
+                         Kids Zone </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
                     aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>

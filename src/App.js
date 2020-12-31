51cfae1421efa92d61399f4889db60a6955c25f5
bsmagennis42 <bsmagennis42@gmail.com>
2020-12-31 17:27:31 +0000

    removed body as router does this job. Tidied route paths.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import Body from "./Components/Body/Body";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Classes } from "./Components/Classes/Classes";
 import Availability from "./Components/Availability/Availability";
 import SessionOverviewDance from "./Components/SessionOverview/SessionOverviewDance";
 import SessionOverviewDrum from "./Components/SessionOverview/SessionOverviewDrum";
+import ActivityListScreen from "./Components/ActivityListScreen/ActivityListScreen";
+import HomeScreen from "./Components/HomeScreen/HomeScreen";
 
 function App() {
   return (
@@ -15,14 +16,10 @@ function App() {
       <Header />
       <Router>
         <Switch>
-          <Route
-            path="/"
-            exact
-            render={(props) => <Body {...props} page="HomeScreen" />}
-          />
+          <Route path="/" exact component={HomeScreen} />
           <Route path="/classes" exact component={Classes} />
           <Route path="/availability" exact component={Availability} />
-          <Route path="/activities" exact render={(props) => <Body {...props} page="ActivityListScreen" />} />         
+          <Route path="/activities" exact component={ActivityListScreen} />         
           <Route exact path="/activities/dance" component={SessionOverviewDance} />
           <Route exact path="/activities/drum" component={SessionOverviewDrum} />
         </Switch>

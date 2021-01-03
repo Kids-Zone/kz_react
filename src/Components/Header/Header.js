71fc40d1a298942f920530fe769000178cc1a3bd
shraddha kapse <shraddha.kapse@gmail.com>
2021-01-03 11:37:15 +0000

    removing .eslintcache

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="">
+        <Nav className="ml-auto">
         <Nav.Link componentClass={Link}  href="/">
             Home
         </Nav.Link>

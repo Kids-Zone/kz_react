b40405a8c2939989fab54f0f6675f4a4cef78a88
bsmagennis42 <bsmagennis42@gmail.com>
2021-01-26 16:18:14 +0000

    changed location of .env - working now

--- src/auth/auth0-provider-with-history.js
+++ src/auth/auth0-provider-with-history.js
@@ -3,14 +3,12 @@ import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const domain = 'dev-mhtjevle.us.auth0.com'//process.env.REACT_APP_AUTH0_DOMAIN;
-   const clientId = 'E3NL2XRqGqZWWyhY2QkxDjnDT5jHXVVK'//process.env.REACT_APP_AUTH0_CLIENT_ID;
-
-  // const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
   const history = useHistory();
- 
+
   const onRedirectCallback = (appState) => {
     history.push(appState?.returnTo || window.location.pathname);
   };
@@ -18,11 +16,10 @@ const Auth0ProviderWithHistory = ({ children }) => {
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.origin+'/PostLogin'}
+      redirectUri={window.location.origin + "/PostLogin"}
       onRedirectCallback={onRedirectCallback}
       audience="https://dev-mhtjevle.us.auth0.com/api/v2/"
       scope="read:current_user update:current_user_metadata"
-
     >
       {children}
     </Auth0Provider>
